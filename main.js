7223c87a6c63f5266eb2c7f120eac0dd851dc0a9
pratiklp <pradhan.pratik@hotmail.com>
2024-10-05 22:00:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,124 +11,78 @@ let container;
 let camera, scene, raycaster, renderer;
 let controller, controllerGrip;
 let geometry, texture, material;
-let cube;
+let room;
+var object;
+let INTERSECTED;
 
-container = document.createElement('div');
-document.body.appendChild(container);
+init();
+function init(){
+    container = document.createElement('div');
+    document.body.appendChild(container);
 
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0x505050);
-camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-renderer = new THREE.WebGLRenderer({
-    antialias : true,
-    alpha: false,
-});
-renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerWidth,
+        0.1,
+        1000,
+    );
+    scene.add(camera);
+    camera.position.z = 5;
+    
+    texture = new THREE.TextureLoader();
+    object = {
+        item1 : new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1,), 
+            new THREE.MeshBasicMaterial({
+                // color : 0x00ff00,
+                map : texture.load('images/logo_copilot.jpg'),
+            })
+        ),
+    };
+    scene.add(object.item1);
+    
+    raycaster = new THREE.Raycaster();
+    
+    renderer = new THREE.WebGLRenderer({
+        antialias : true,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.xr.enabled = true;
+    container.appendChild(renderer.domElement);
 
-geometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1
-);
-texture = new THREE.TextureLoader();
-material = new THREE.MeshBasicMaterial(
-    {
-        // color : 0x00ff00
-        map : texture.load('images/logo_copilot.jpg'),
-    }
-);
-cube = new THREE.Mesh(
-    geometry,
-    material
-);
-scene.add(cube);
-camera.position.z = 5;
+    controller = renderer.xr.getController(0);
+    scene.add(controller);
 
-raycaster = new THREE.Raycaster();
+    const controllerModelFactory = new XRControllerModelFactory();
+    controllerGrip = renderer.xr.getControllerGrip(0);
+    controllerGrip.add(controllerModelFactory.createControllerModel(controllerGrip));
+    scene.add(controllerGrip);
 
-renderer = new THREE.WebGLRenderer({
-    antialias : true,
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-renderer.xr.enabled = true;
-container.appendChild(renderer.domElement);
+    window.addEventListener('resize', onWindowResize);
 
-controller = renderer.xr.getController(0);
-controller.addEventListener('connect', function(event){
-    this.add(buildController(event.data));
-});
-controller.addEventListener('disconnected', function(){
-    this.remove(this.children[0]);
-});
-scene.add(controller);
-
-const controllerModelFactory = new XRControllerModelFactory();
-controllerGrip = renderer.xr.getControllerGrip(0);
-controllerGrip.add(
-    controllerModelFactory.createControllerModel(controllerGrip)
-);
-scene.add(controllerGrip);
-
-window.addEventListener('resize', onWindowResize);
-
-document.body.appendChild(XRButton.createButton(renderer, {
-    'optionalFeatures' : ['depth-sensing'],
-    'depthSensing': {
-        'usagePreference' : ['gpu-optimized'],
-        'dataFormatPreference' : []
-    }
-}));
-
-function buildController( data ) {
-
-    let geometry, material;
-
-    switch ( data.targetRayMode ) {
-
-        case 'tracked-pointer':
-
-            geometry = new THREE.BufferGeometry();
-            geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
-            geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
-
-            material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
-
-            return new THREE.Line( geometry, material );
-
-        case 'gaze':
-
-            geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
-            material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
-            return new THREE.Mesh( geometry, material );
-
-    }
-
+    document.body.appendChild(XRButton.createButton( renderer, {
+        'optionalFeatures': ['depth-sensing'],
+        'depthSensing': {
+            'usagePreference' : [
+                'gpu-optimized'
+            ],
+            'dataFormatPreference' : []
+        }
+    }));
 }
 
-function onWindowResize() {
-
+function onWindowResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    camera.updateProjectMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate(){
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(
-        scene,
-        camera
-    );
+    object.item1.rotation.x += 0.01;
+    object.item1.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
